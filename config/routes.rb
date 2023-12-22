5a1f591c17e9d1be2fcda03dd724ace87715dae5
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-12-23 05:55:54 +1000

    added show route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   get "/exercises" => "exercises#index"
-  get "/exercises" => "exercises#show"
+  get "/exercises/:id" => "exercises#show"
   post "/exercises" => "exercises#create"
 end
