b92be78951bd27d4d2ba4f5dc79de8cec393024d
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-12-22 13:00:52 +1000

    created tings to seed

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,3 @@
 class Exercise < ApplicationRecord
+  belongs to :routine
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :routines
+  validates :email, presence: true, uniqueness: true
 end
