63a0899c174cd4f880b6b7a3a19ed4573f45661c
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-12-28 07:05:23 +1000

    uncommented admin permissions

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,5 @@
 class ExercisesController < ApplicationController
-  # before_action :authenticate_admin, except: [:index, :show]
+  before_action :authenticate_admin, except: [:index, :show]
 
   def index
     @exercises = Exercise.all
